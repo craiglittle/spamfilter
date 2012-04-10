5c434bf4a3731bdde0c830287d3b49147c5e0fba
Craig Little <craiglttl@gmail.com>
2012-04-10 14:12:19 -0700

    hooking up fisher

--- lib/base.rb
+++ lib/base.rb
@@ -3,7 +3,7 @@ module SpamFilter
   class << self
 
     def run
-      token_spamicities = spamicities_file ? load_token_spamicities : calculate_token_spamicities
+      token_spamicities = spamicities_file ? load_token_spamicities : calculate_spamicities
       spam_results = test_emails(:spam, token_spamicities)
       ham_results  = test_emails(:ham, token_spamicities)
 
@@ -37,65 +37,73 @@ module SpamFilter
       results.select { |email| email[:accurate?] == false }.size
     end
 
-    def parse_corpus(emails)
-      emails.inject({}) { |tokens, email| tokenize_email(email, tokens); tokens }
+    def parse_corpus(corpus_type, token_map, email_count)
+      corpus = (corpus_type == :spam ? spam_corpus : ham_corpus)
+      corpus.each do |email| 
+        email_count[corpus_type] += 1
+        tokens = tokenize_email(email)
+        record_tokens(tokens, corpus_type, token_map)
+      end
     end
     
-    def tokenize_email(email, tokens = {})
+    def tokenize_email(email)
       email_text = File.read(email).encode('UTF-16BE', 
                                            :invalid => :replace, 
                                            :replace => '?').encode('UTF-8')
-      raw_tokens = tokenize_text(email_text)
-      hashify_tokens(raw_tokens, tokens)
+      tokenize_text(email_text)
     end
 
     def tokenize_text(text)
       cleaned_text = text.gsub(REGEXP[:html_comment], '')
       tokens = [:ip_url, :core_token].inject([]) { |tokens, regexp| tokens += cleaned_text.scan(REGEXP[regexp]).flatten }
       token_pairs = cleaned_text.scan(REGEXP[:token_pair])
-      str_pairs = token_pairs.map { |pair| pair.join(' ') }
-      tokens += str_pairs
+      tokens += token_pairs.map { |pair| pair.join(' ') }
+      tokens.uniq
     end
 
-    def hashify_tokens(raw_tokens, tokens = {})
-      raw_tokens.inject(tokens) do |tokens, token| 
-        next tokens if token.match(/^\d+\s|\s\d+$|^\d+$/) # ignore pure numbers
-        tokens[token] ? tokens[token] += 1 : tokens[token] = 1; tokens
+    def record_tokens(tokens, corpus_type, token_map)
+      tokens.each do |token| 
+        next if token.match(/^\d+\s|\s\d+$|^\d+$/) # ignore pure numbers
+        token_map[token] = { :spam => 0, :ham => 0 } if token_map[token].nil?
+        token_map[token][corpus_type] += 1
       end
     end
 
-    def calculate_token_spamicities
-      puts 'Calculating token spamicities...'
-
-      spam_tokens      = parse_corpus(spam_corpus)
-      spam_email_count = spam_corpus.length
+    def calculate_spamicities
+      token_map   = {}
+      email_count = { :spam => 0, :ham => 0 }
+      parse_corpus(:spam, token_map, email_count)
       puts 'Done with spam.'
-      ham_tokens       = parse_corpus(ham_corpus)
-      ham_email_count  = ham_corpus.length
+      parse_corpus(:ham, token_map, email_count)
       puts 'Done with ham.'
 
-      token_spamicities = {}
-      unique_tokens = (spam_tokens.keys + ham_tokens.keys).uniq
+      token_map.each do |token, _|
+        spam_count = token_map[token][:spam]
+        ham_count  = token_map[token][:ham]
+        data_points  = spam_count + ham_count
 
-      unique_tokens.each do |token|
-        spam_token_count = spam_tokens[token] || 0
-        ham_token_count  = (ham_tokens[token] || 0) * ham_token_multiplier
-
-        unless spam_token_count + ham_token_count < 10
-          spam_pct      = [1.0, spam_token_count / spam_email_count.to_f].min
-          ham_pct       = [1.0, ham_token_count / ham_email_count.to_f].min
-          bayes_pct     = (spam_pct / (spam_pct + ham_pct)).round(2)
-          adj_bayes_pct = [0.01, [0.99, bayes_pct].min].max
-
-          token_spamicities[token] = adj_bayes_pct
+        if data_points < 5
+          token_map.delete(token)
+          next
         end
+
+        spam_freq  = spam_count / [1, email_count[:spam]].max.to_f
+        ham_freq   = ham_count / [1, email_count[:ham]].max.to_f
+        basic_prob = spam_freq / (spam_freq + ham_freq)
+
+        # frequency adjustment
+        assumed_prob = 0.5
+        strength     = 1
+        spamicity = (strength * assumed_prob + data_points * basic_prob) / (strength + data_points)
+
+        token_map[token][:spamicity] = spamicity
       end
       
-      File.open('spamicities.yml', 'w') { |f| f.write(token_spamicities.to_yaml) }
-      token_spamicities
+      File.open('spamicities.yml', 'w') { |f| f.write(token_map.to_yaml) }
+      token_map
     end
-    
-    def test_emails(corpus_type, token_spamicities)
+
+    def score_emails(corpus_type, token_spamicities)
       print "Testing #{corpus_type.to_s} emails..."
       emails = corpus_type == :spam ? spam_test : ham_test
       test_output = emails.inject([]) { |output, email| output << test_single_email(email, corpus_type, token_spamicities) }
@@ -103,7 +111,7 @@ module SpamFilter
       test_output
     end
     
-    def test_single_email(email, corpus_type, token_spamicities)
+    def score_email(email, corpus_type, token_spamicities)
       tokens = tokenize_email(email)
       tokens.each do |k, v| 
         spamicity = token_spamicities[k] || 0.5
@@ -128,6 +136,47 @@ module SpamFilter
 
       # { :path => email, :spamicity => email_spamicity, :top_tokens => top_tokens, :verdict => verdict, :accurate? => verdict == corpus_type }
       { :accurate? => verdict == corpus_type }
+    end
+
+    def score(tokens, token_spamicities)
+      spam_probs = []
+      ham_probs  = []
+      prob_count = 0
+
+      tokens.each do |token|
+        if interesting?(token, token_spamicities)
+          spam_prob = token_spamicities[token][:spamicity]
+          spam_probs << spam_prob
+          ham_probs << 1.0 - spam_prob
+          prob_count += 1
+        end
+      end
+
+      h = 1 - fisher(spam_probs, prob_count)
+      s = 1 - fisher(ham_probs, prob_count)
+
+      (s + (1 - h)) / 2.0
+    end
+    
+    def interesting?(token, token_spamicities)
+      abs_spamicity = (0.5 - token_spamicities[token][:spamicity]).abs
+      abs_spamicity > 0.4
+    end
+
+    def fisher(probs, prob_count)
+      chi = -2 * probs.inject(0) { |total, prob| total += prob.log }
+      df  = 2 * prob_count 
+      inverse_chi_square(chi, df)
+    end
+
+    def inverse_chi_square(chi, df)
+      m = chi / 2.0
+      sum = term = math.exp(-m)
+      (1..df/2).each do |i|
+        term *= m / i
+        sum += term
+      end
+      [1.0, sum].min
     end
   end
 end
--- spamfilter.rb
+++ spamfilter.rb
@@ -5,14 +5,14 @@ require_relative './lib/config'
 require_relative './lib/base'
 
 SpamFilter.configure do |config|
-  config.spam_corpus          = 'corpus/spam_corpus'
-  config.ham_corpus           = 'corpus/ham_corpus'
+  config.spam_corpus          = 'corpus/sa_spam'
+  config.ham_corpus           = 'corpus/sa_ham'
   config.spam_test            = 'corpus/spam_test'
   config.ham_test             = 'corpus/ham_test'
-  config.spamicities_file     = 'spamicities.yml'
+  # config.spamicities_file     = 'spamicities.yml'
   config.top_tokens_list_size = 27
   config.spam_threshold       = 0.9
   config.ham_token_multiplier = 1
 end
 
-SpamFilter.run
+SpamFilter.calculate_spamicities
